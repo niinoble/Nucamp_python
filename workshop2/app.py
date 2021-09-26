be12bd8e064000b7184a5a838d8448d07bee55dc
noble amarteifio <gbxn@yahoo.com>
2021-09-26 10:16:56 -0400

    Assingment Week 2 Submission

--- workshop2/app.py
+++ workshop2/app.py
@@ -14,25 +14,42 @@ def atm_menu(name):
 
 
 print("          === Automated Teller Machine ===          ")
-name = input("Enter name to register: ")
-
-pin = int(input("Enter PIN: "))
 balance = 0
 
+#Bonus Task: Checking for maximum length
+while True:
+    name = input("Enter name to register: ")
+    if len(name) < 10:
+        break
+    else:
+        print("The maximum name length is 10 characters.")
+
+#Bonus Task: Checking for required PIN length
+while True:
+    pin = (input("Enter PIN: "))
+    if len(pin) == 4:
+        break
+    else:
+        print("PIN must contain 4 numbers")
+
+
 print(name," has been registered with a starting balance of $"+str(0),"\n")
+
 
 while True:
     print("          === Automated Teller Machine ===          ")
     print("LOGIN")
     
-    name_to_validate = input("Enter name to register: ")
-    pin_to_validate = int(input("Enter PIN: "))
+    name_to_validate = input("Enter name to login: ")
+    pin_to_validate = (input("Enter PIN: "))
     if name_to_validate == name and pin_to_validate ==pin:
+        
         print("Login successful! \n")
         break
     else:
         print("Invalid credentials! \n")
 
+#Task 5 Using Banking Package: ATM Menu Options
 while True:
         atm_menu(name)
         option = input("Chooose an option:")
@@ -49,8 +66,10 @@ while True:
             balance = account.withdraw(balance)
             
 
+            
+
         else:
             account.logout(name)
             break
 
-        print("Current Balance: $"+str(balance))
+        print("Current Balance: $"+str(balance)+ "\n")
--- workshop2/banking_pkg/account.py
+++ workshop2/banking_pkg/account.py
@@ -1,20 +1,36 @@
+'''
+Task 4
+-------
+Account Functions.
+
+
+'''
+
+#Show function
 def show_balance(balance):
     balance = float(balance)
     return balance
 
-
+#Deposit Function
 def deposit(balance):
     amount = float(input("Enter amount to deposit: "))
     balance = amount + balance
     return balance
 
-
+#Witdraw Function
 def withdraw(balance):
     amount = float(input("Enter amount to withdraw: "))
-    balance = balance - amount
+
+#Bonus Task 3 (Check withdrawal function)
+    while True:
+        if amount > balance:
+                    print("Where are you going to get that kind of money?")
+                    break
+        else:
+            balance = balance - amount
 
     return balance
 
-
+#Logout Function
 def logout(name):
     print("Goodbye", name + "!")
